c8f183936fb196d9662ffb5aefdea0b1e4d5be8f
geoffstone002 <geoff.stone002@gmail.com>
2013-01-24 20:12:44 +1300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,6 +21,7 @@ describe "UserPages" do
   end
 
   describe "signup" do
+
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -29,16 +30,17 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
     end
-
-    describe "after submission" do
-      before { click_button submit }
-
-      it { should have_selector('title', text: 'Sign up') }
-      it { should have_content('error') }
-    end
-
+    
     describe "with valid information" do
+      
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
@@ -46,14 +48,16 @@ describe "UserPages" do
         fill_in "Confirmation", with: "foobar"
       end
 
-        describe "after saving the user" do
-          before { click_button submit }
-          let(:user) { User.find_by_email('user@example.com') }
+      describe "after saving the user" do
+        before { click_button submit }
+        
+        let(:user) { User.find_by_email('user@example.com') }
 
-          it { should have_selector('title', text: user.name) }
-          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-        end
-
+        it { should have_selector('title', text: user.name) }
+        it { should have_link('Sign out') }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+        
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
